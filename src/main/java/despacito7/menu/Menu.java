b840aae37d4c0d7eac719c903f6a17b7acddd64b
Steve Scruff <mutantpiggiegolems@gmail.com>
2022-12-17 12:04:07 -0800

    actually fix menus

--- src/main/java/despacito7/menu/Menu.java
+++ src/main/java/despacito7/menu/Menu.java
@@ -5,16 +5,13 @@ import java.awt.Graphics2D;
 import java.util.HashSet;
 import java.util.Set;
 
-import despacito7.Constants;
 import despacito7.DrawingCanvas;
 import despacito7.util.Drawable;
-import despacito7.util.Utils;
 
-import java.awt.Image;
 import java.awt.Point;
 
 public abstract class Menu {
-    public static final Menu cornerMenu = new RotaryMenu(new Point(0, 0));
+    public static final RotaryMenu cornerMenu = new RotaryMenu(new Point(0, 0));
     protected Set<Button> buttons;
 
     public abstract void update();
@@ -53,65 +50,6 @@ class DialogueMenu extends Menu implements Drawable {
         @Override
         public void draw(Graphics2D g) {
             
-        }
-    }
-}
-
-class RotaryMenu extends Menu implements Drawable {
-    private Point origin;
-    private int radius = 32;
-    protected boolean expanded = false;
-
-    public RotaryMenu(Point origin) {
-        this.origin = origin;
-        this.buttons = Set.of(
-            new RotaryButton(Constants.Images.MonsterIcon, 20), // Monsters
-            new RotaryButton(Constants.Images.ItemsIcon, 40), // Items
-            new RotaryButton(Constants.Images.SettingsIcon, 60), // Settings
-            new RotaryButton(Constants.Images.ExitIcon, 80) // Exit
-        );
-    }
-
-    public void expand() {
-        this.expanded = !this.expanded;
-        this.buttons.forEach(b->((RotaryButton)b).expand());
-    }
-
-    public void update() {
-        for (Button b : this.buttons) ((RotaryButton)b).update();
-    }
-
-    private class RotaryButton implements Button {
-        private static int size = 12;
-        private Image icon;
-        private double direction;
-        private double animprog = 0;
-        private double[] vectors;
-
-        private RotaryButton(Image icon, double degrees) {
-            this.icon = icon;
-            this.direction = Math.toRadians(degrees);
-            this.vectors = new double[]{
-                Math.cos(this.direction) * radius, 
-                Math.sin(this.direction) * radius
-            };
-        }
-
-        private void expand() {
-            animprog = 0;
-        }
-
-        public void update() {
-            animprog += 0.01;
-            animprog = Math.min(animprog, 1);
-        }
-
-        public void draw(Graphics2D g) {
-            double coeff = Utils.easeOutBack(animprog);
-            int x = (int)Math.round(this.vectors[0] * coeff + origin.getX());
-            int y = (int)Math.round(this.vectors[1] * coeff + origin.getY());
-            g.drawOval(x, y, size, size);
-            g.drawImage(this.icon, x, y, null);
         }
     }
 }
